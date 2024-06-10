0521e8e1013a76acc11d677d34c0c9524cb56291
WijiYvv <95729854+WijiYvv@users.noreply.github.com>
2024-06-11 00:00:12 +0800

    Add files via upload

---
+++ titor.py
@@ -0,0 +1,25 @@
+def toNomal(titor):
+    titor = titor.upper()
+    res = []
+    for i in range(0, len(titor), 2):
+        num_hex = titor[i : i + 2]  # 每2个字符转换为1个十六进制数字
+        num_ord = int(num_hex, 16)  # 把十六进制转换为十进制
+        letter = chr(num_ord)  # 按照ascii表，把十进制数字转换为字母
+        res.append(letter)
+    return "".join(res)
+
+
+def toTitor(nomal):
+    nomal = list(nomal)
+    res = []
+    for i in range(len(nomal)):
+        num_ord = ord(nomal[i])  # 把每个字母转换为十进制数字
+        num_hex = hex(num_ord)[2:]  # 将十进制数字转换为十六进制
+        res.append(num_hex)
+    return "".join(res).upper()
+
+
+titor = "546865496E6469616E4F6365616E77696C6C6265726F756768"
+nomal = "I'mJohnTitor"
+print(toNomal(titor))
+print(toTitor(nomal))
